025b15678f2883ef0550a8e07d1dc9f4a7edf7c1
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2022-12-18 22:01:03 +0530

    Ensambled svm and nb models

--- ml_models/models_server/main.py
+++ ml_models/models_server/main.py
@@ -38,15 +38,31 @@ def get_fact_validation(fact: str):
 
     pred_lstm = fact_validator_lstm(fact, model, tokenizer, 300)
 
+    # SVM Model
+    (svm_model, svm_vectorizer) = load_model_vectorizer_svm("../saved_models/svm_classifier.pkl", "../saved_models/vectorizer_SVM_NB.pickle")
+    pred_svm = fact_validator_svm(fact, svm_model, svm_vectorizer)
+
+    # Naive Bayes Model
+    (nb_model, nb_vectorizer) = load_model_vectorizer_svm("../saved_models/bernoulli_nb_classifier.pkl", "../saved_models/vectorizer_SVM_NB.pickle")
+    pred_nb = fact_validator_nb(fact, nb_model, nb_vectorizer)
+
     # Ensembling Result
     pred_lstm = float(pred_lstm)
     pred_pac = float(pred_pac[0])
+    pred_svm = float(pred_svm)
+    pred_nb = float(pred_nb)
 
     if(pred_pac == 1.0):
         pred_pac = 0.75
     else :
         pred_pac = 0.25
+    
+    if(pred_svm==1.0):
+        pred_svm = 0.75
+    else:
+        pred_svm = 0.25
 
-    ensembled_result = (pred_lstm + pred_pac)/2
+
+    ensembled_result = (pred_lstm + pred_pac + pred_svm + pred_nb)/4
 
     return {"trust_score": str(ensembled_result)}
